be497b47de800ad585eacc35ccad7fb1fa97c1ad
sany45 <sanybhuiyan97@gmail.com>
2024-01-27 02:48:48 +0600

    task3

---
+++ task3.js
@@ -0,0 +1,10 @@
+var bangla = 78;
+var english = 88;
+var biology = 90;
+var cemechstry = 85;
+var math = 80;   
+var totalNumber = bangla + english + biology + cemechstry + math;
+var totalSub = 5;
+var avarege = totalNumber / totalSub;
+
+console.log(avarege)
