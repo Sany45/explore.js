b8a29ebf042c4075792ce70f759ba8ca26273056
sany45 <sanybhuiyan97@gmail.com>
2024-01-27 02:31:26 +0600

    start-practise-task

---
+++ task.js
@@ -0,0 +1,9 @@
+var iHave = 1000;
+var apple = 200;
+var orange = 500;
+var choclate = 200;
+var totalPrice = apple + orange + choclate;
+var dokandarBackme = iHave - totalPrice;
+
+
+console.log(dokandarBackme);
